54695eb5e1058ca765b15f0fe816d47813ad1489
Lidor Batino <lidorb28@gmail.com>
2019-11-20 15:09:22 +0200

    Updates

---
+++ precache-manifest.03ca3570ebedf77d4385755d373f0ae0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc9db4653643d00c91cf5ac7dd55cc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eae41267db9b36cb253d",
+    "url": "/static/css/main.0bff623c.chunk.css"
+  },
+  {
+    "revision": "1b7d68bba2ff7d539318",
+    "url": "/static/js/2.6af18660.chunk.js"
+  },
+  {
+    "revision": "eae41267db9b36cb253d",
+    "url": "/static/js/main.df39d805.chunk.js"
+  },
+  {
+    "revision": "b586dfc11206f938bc13",
+    "url": "/static/js/runtime-main.76a07cac.js"
+  }
+]);
