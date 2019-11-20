9dd9739031ffc1ce898d9a488e64ce1f6248ea18
Lidor Batino <lidorb28@gmail.com>
2019-11-20 23:09:04 +0200

    Updates

---
+++ precache-manifest.0dc8c034faeab598a8a1c3e548397ef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e8d58ab6961bf719ab7d604cc90326",
+    "url": "/instagram-ui/index.html"
+  },
+  {
+    "revision": "58d088e08e402a4abd40",
+    "url": "/instagram-ui/static/css/main.0bff623c.chunk.css"
+  },
+  {
+    "revision": "879fd528ccc0bbe40b23",
+    "url": "/instagram-ui/static/js/2.8c53cde6.chunk.js"
+  },
+  {
+    "revision": "58d088e08e402a4abd40",
+    "url": "/instagram-ui/static/js/main.a715e93c.chunk.js"
+  },
+  {
+    "revision": "fd329e52972bd349eb48",
+    "url": "/instagram-ui/static/js/runtime-main.9b17314b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc14c90770146446426961e64fcc4839.js"
+  "/instagram-ui/precache-manifest.0dc8c034faeab598a8a1c3e548397ef2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/instagram-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
